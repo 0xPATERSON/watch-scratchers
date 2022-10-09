4521814bcdd8aec555724f18d1814f2fb5cb51bf
Daniel Lin <dlin@USNYRWASAADAO4.localdomain>
2022-10-09 17:29:18 -0400

    rest of the renderers

--- src/interfaces/IWatchScratchersWatchCaseRenderer.sol
+++ src/interfaces/IWatchScratchersWatchCaseRenderer.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 interface IWatchScratchersWatchCaseRenderer {
-    enum CaseType { PP, AP, SUB, YACHT, DJ, OP, DD, EXP, VC, GS, TANK, TANK_F, PILOT, AQ, SENATOR }
+    enum CaseType { PP, AP, SUB, YACHT, DJ, OP, DD, DD_P, EXP, VC, GS, TANK, TANK_F, PILOT, AQ, SENATOR }
 
     error WrongCaseRendererCalled();
 
